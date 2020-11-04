a603a030c33faf80c21155ac0dd1096fb614e160
Jorgezero4 <43280251+Jokerzero4@users.noreply.github.com>
2020-11-04 17:58:45 -0600

    Add files via upload

---
+++ assets/assets/js/main.js
@@ -0,0 +1,121 @@
+
+(function ($) {
+	"use strict";
+
+	var nav = $('nav');
+	var navHeight = nav.outerHeight();
+
+	$('.navbar-toggler').on('click', function () {
+		if (!$('#mainNav').hasClass('navbar-reduce')) {
+			$('#mainNav').addClass('navbar-reduce');
+		}
+	});
+	
+	// START PRELOADED
+    $(window).on('load', function() {
+        $('.preloader').fadeOut();
+        $('.preloader-inner').delay(350).fadeOut('slow');
+    });
+	
+	// Navbar Menu Reduce 
+	$(window).trigger('scroll');
+	$(window).on('scroll', function () {
+		var pixels = 50;
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce');
+			$('.navbar-expand-md').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-md').addClass('navbar-trans');
+			$('.navbar-expand-md').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+
+	// Back to top button 
+		// Go to Top
+        $(function(){
+            // Scroll Event
+            $(window).on('scroll', function(){
+                var scrolled = $(window).scrollTop();
+                if (scrolled > 300) $('.back-to-top').addClass('active');
+                if (scrolled < 300) $('.back-to-top').removeClass('active');
+            });  
+            // Click Event
+            $('.back-to-top').on('click', function() {
+                $("html, body").animate({ scrollTop: "0" },  500);
+            });
+        });
+        
+        
+
+	//  Star Scrolling nav
+	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - navHeight + 30)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll').on("click", function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: navHeight
+	});
+	
+	   //SUPER BG SLİDER
+		$('#slides').superslides({
+		  animation: 'fade',
+		  play: 3000
+		});   
+    
+	// Testimonials owl
+	$('#testimonial-slide').owlCarousel({
+		margin: 5,
+		autoplay: true,
+		center: true,
+		autoplayTimeout: 4000,
+		nav: false,
+		smartSpeed: 1000,
+		dots: false,
+		autoplayHoverPause: true,
+		loop: true,
+        responsiveClass:true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 2
+			}
+		}
+	});
+	
+        // Zoom Gallery
+        $('.popup-btn').magnificPopup({
+            type: 'image',
+            gallery:{
+                enabled:true
+            }
+        });
+
+	
+})(jQuery);
